452c2ef46eb350929b9131933b27261b6f16c65b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-05-06 01:10:22 +0300

    registration not complete yet :3

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,82 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
 
-export default function Dashboard() {
+const formSchema = z.object({
+    first_name: z
+        .string({
+            message: 'First name is required',
+        })
+        .min(1),
+    last_name: z
+        .string({
+            message: 'Last name is required',
+        })
+        .min(1),
+    email: z
+        .string({
+            message: 'Email is required',
+        })
+        .email(),
+    password: z
+        .string({
+            message: 'Password is required',
+        })
+        .min(8, { message: 'Password must be at least 8 characters' }),
+    confirm_password: z
+        .string({
+            message: 'Confirm password is required',
+        })
+        .min(8, { message: 'Password must be at least 8 characters' }),
+});
+
+export default function Register() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    });
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+        // post to the api, get the response or the errors and set them accordingly
+        fetch('http://40.118.26.223/user/registration/', {
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return res.json().then((data) => {
+                        console.log('hopa');
+                        console.log(data);
+                        // throw new Error(data.error);
+                    });
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
     return (
         <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[89.6vh]">
             <div className="flex items-center justify-center py-12">
@@ -16,7 +87,115 @@ export default function Dashboard() {
                             Enter your information to create an account
                         </p>
                     </div>
-                    <div className="grid gap-4">
+                    <Form {...form}>
+                        <div className="">
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                <div className="flex flex-col space-y-4 w-full">
+                                    <div className="grid grid-cols-2 gap-4">
+                                        {/* First Name */}
+                                        <FormField
+                                            control={form.control}
+                                            name="first_name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        First Name
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            placeholder=""
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* Last Name */}
+                                        <FormField
+                                            control={form.control}
+                                            name="last_name"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>
+                                                        Last Name
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            placeholder=""
+                                                            {...field}
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+
+                                    {/* Email */}
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder=""
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    {/* Password */}
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        type="password"
+                                                        placeholder=""
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    {/* Confirm Password */}
+                                    <FormField
+                                        control={form.control}
+                                        name="confirm_password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>
+                                                    Confirm Password
+                                                </FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        type="password"
+                                                        placeholder=""
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    {/* Submit Button */}
+                                    <Button className="w-fit" type="submit">
+                                        Submit
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
+                    </Form>
+                    {/* <div className="grid gap-4">
                         <div className="grid grid-cols-2 gap-4">
                             <div className="grid gap-2">
                                 <Label htmlFor="first-name">First name</Label>
@@ -51,7 +230,7 @@ export default function Dashboard() {
                         <Button type="submit" className="w-full">
                             Create an account
                         </Button>
-                    </div>
+                    </div> */}
                     <div className="mt-4 text-center text-sm">
                         Already have an account?{' '}
                         <Link href="login" className="underline">
