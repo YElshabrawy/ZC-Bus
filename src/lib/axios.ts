0bfdc474d73c82b197a4025cb3685ccaa3c9334b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-05-19 09:26:48 +0300

    No ENV var for base URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = 'https://youssef.software';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
